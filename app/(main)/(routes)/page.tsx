ca1e96071380a3ee45c519e1e0e0de7e6d559ae5
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-04-29 15:20:39 +0200

    added authentication

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,18 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
  return (
-  <div>
-   <p className="text-3xl font-bold text-indigo-500">Discord clone</p>
-  </div>
+  <main className="h-full">
+   <SignedOut>
+    <div className="flex h-full items-center justify-center ">
+     <div className="bg-indigo-500 p-2 rounded-md font-bold text-white">
+      <SignInButton />
+     </div>
+    </div>
+   </SignedOut>
+   <SignedIn>
+    <UserButton />
+   </SignedIn>
+  </main>
  );
 }
