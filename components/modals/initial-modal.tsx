399f39b26daf465bd4f9009a50527c49a8a18ca1
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-04-30 16:04:56 +0200

    added ability to upload server image

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -22,6 +22,7 @@ import {
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
+import { FileUpload } from "../file-upload";
 
 const formSchema = z.object({
  name: z.string().min(1, {
@@ -72,6 +73,23 @@ export const InitialModal = () => {
       className="space-y-8"
      >
       <div className="space-y-8 px-6">
+       <div className="flex items-center justify-center text-center">
+        <FormField
+         control={form.control}
+         name="imageUrl"
+         render={({ field }) => (
+          <FormItem>
+           <FormControl>
+            <FileUpload 
+            endpoint="serverImage"
+            value={field.value}
+            onChange={field.onChange} 
+            />
+           </FormControl>
+          </FormItem>
+         )}
+        />
+       </div>
        <FormField
         control={form.control}
         name="name"
