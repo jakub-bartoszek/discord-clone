289eaa62143a11e10dcd400e7c32a8c3caeb6a09
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-04-30 15:18:10 +0200

    created Dialog component

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
      <ThemeProvider
       attribute="class"
       defaultTheme="dark"
-      enableSystem={false}
+      enableSystem={true}
       storageKey="discord-theme"
      >
       {children}
