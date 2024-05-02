669bd29a1f77beed1c83ec0f1c8b7256e3131bd2
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-02 16:27:09 +0200

    fixed axios post path

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -53,7 +53,7 @@ export const CreateServerModal = () => {
 
  const onSubmit = async (values: z.infer<typeof formSchema>) => {
   try {
-   await axios.post("api/servers", values);
+   await axios.post("/api/servers", values);
 
    form.reset();
    router.refresh;
