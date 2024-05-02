33a436d5b25c6e5544f565540812c78abf12d32d
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-02 16:26:53 +0200

    changed order of imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
