ee8b79017a9279c4c0ad90d2227523ef0a3a6c9b
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-02 14:22:02 +0200

    added ability to create a server

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as z from "zod";
+import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import {
@@ -23,6 +24,7 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
 import { FileUpload } from "../file-upload";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
  name: z.string().min(1, {
@@ -35,6 +37,8 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
  const [isMounted, setIsMounted] = useState(false);
+
+ const router = useRouter();
 
  useEffect(() => {
   setIsMounted(true);
@@ -51,7 +55,15 @@ export const InitialModal = () => {
  const isLoading = form.formState.isSubmitting;
 
  const onSubmit = async (values: z.infer<typeof formSchema>) => {
-  console.log(values);
+  try {
+   await axios.post("api/servers", values);
+
+   form.reset();
+   router.refresh;
+   window.location.reload();
+  } catch (error) {
+   console.log(error);
+  }
  };
 
  if (!isMounted) {
@@ -80,10 +92,10 @@ export const InitialModal = () => {
          render={({ field }) => (
           <FormItem>
            <FormControl>
-            <FileUpload 
-            endpoint="serverImage"
-            value={field.value}
-            onChange={field.onChange} 
+            <FileUpload
+             endpoint="serverImage"
+             value={field.value}
+             onChange={field.onChange}
             />
            </FormControl>
           </FormItem>
