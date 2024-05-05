25fa5c5073d203d1d91ffe0a8550cb4ad19eee7f
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-05 16:22:01 +0200

    changed defaultValue to value

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -61,7 +61,7 @@ export const InviteModal = () => {
       <Input
        disabled={isLoading}
        className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-       defaultValue={inviteUrl}
+       value={inviteUrl}
       />
       <Button
        disabled={isLoading}
