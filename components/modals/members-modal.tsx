013836103a88be8fd7d9c67d0ff8352da6c0e2b4
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-06 12:41:53 +0200

    added ability to kick users

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -48,13 +48,34 @@ export const MembersModal = () => {
  const isModalOpen = isOpen && type === "members";
  const { server } = data as { server: ServerWithMembersWithProfiles };
 
+ const onKick = async (memberId: string) => {
+  try {
+   setLoadingId(memberId);
+   const url = qs.stringifyUrl({
+    url: `/api/members/${memberId}`,
+    query: {
+     serverId: server?.id
+    }
+   });
+
+   const response = await axios.delete(url);
+
+   router.refresh();
+   onOpen("members", { server: response.data });
+  } catch (error) {
+   console.log(error);
+  } finally {
+   setLoadingId("");
+  }
+ };
+
  const onRoleChange = async (memberId: string, role: MemberRole) => {
   try {
    setLoadingId(memberId);
    const url = qs.stringifyUrl({
     url: `/api/members/${memberId}`,
     query: {
-     serverId: server?.id,
+     serverId: server?.id
     }
    });
 
@@ -128,7 +149,7 @@ export const MembersModal = () => {
             </DropdownMenuPortal>
            </DropdownMenuSub>
            <DropdownMenuSeparator />
-           <DropdownMenuItem>
+           <DropdownMenuItem onClick={() => onKick(member.id)}>
             <Gavel className="w-4 h-4 mr-2" />
             Kick
            </DropdownMenuItem>
