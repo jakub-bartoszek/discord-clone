683069ad502cd99549880612713f8db91b9aa737
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-10 22:06:38 +0200

    changed light mode bg color

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -67,7 +67,7 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
  const role = server.members.find((member) => member.profileId === profile.id)?.role;
 
  return (
-  <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
+  <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-zinc-100">
    <ServerHeader
     server={server}
     role={role}
