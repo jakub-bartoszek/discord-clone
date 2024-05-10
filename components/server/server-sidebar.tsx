9e915ef8ed2ece08075499516841609c985c7592
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-10 15:29:34 +0200

    created text channels section

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,12 +1,16 @@
 import { ChannelType, MemberRole } from "@prisma/client";
 import { redirect } from "next/navigation";
+import { Hash, Mic, ShieldAlert, ShieldCheck } from "lucide-react";
 
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { Hash, Mic, ShieldAlert, ShieldCheck } from "lucide-react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+
 import { ServerHeader } from "./server-header";
 import { ServerSearch } from "./server-search";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ServerSection } from "./server-section";
+import { ServerChannel } from "./server-channel";
 
 interface ServerSidebarProps {
  serverId: string;
@@ -101,6 +105,25 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
       ]}
      />
     </div>
+    <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
+    {!!textChannels?.length && (
+     <div className="mb-2">
+      <ServerSection
+       sectionType="channels"
+       channelType={ChannelType.TEXT}
+       role={role}
+       label="Text channels"
+      />
+      {textChannels.map((channel) => (
+       <ServerChannel
+        key={channel.id}
+        channel={channel}
+        role={role}
+        server={server}
+       />
+      ))}
+     </div>
+    )}
    </ScrollArea>
   </div>
  );
