167e7fb594f7a2388a5643eb864d05036fdfc04d
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-10 16:00:19 +0200

    passing channel type to create channel modal

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -33,7 +33,7 @@ export const ServerSection = ({
      side="top"
     >
      <button
-      onClick={() => onOpen("createChannel")}
+      onClick={() => onOpen("createChannel", { channelType })}
       className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
      >
       <Plus className="w-4 h-4" />
@@ -42,7 +42,7 @@ export const ServerSection = ({
    )}
    {role === MemberRole.ADMIN && sectionType === "members" && (
     <ActionTooltip
-     label="Create channel"
+     label="Manage members"
      side="top"
     >
      <button
