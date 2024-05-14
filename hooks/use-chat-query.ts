64216ebd616c2f2dcbf2a7be49192fd99335c713
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-14 17:21:52 +0200

    added condition to refetchInterval

--- hooks/use-chat-query.ts
+++ hooks/use-chat-query.ts
@@ -41,7 +41,7 @@ export const useChatQuery = ({
     queryKey: [queryKey],
     queryFn: fetchMessages,
     getNextPageParam: (lastPage) => lastPage?.nextCursor,
-    refetchInterval: 500
+    refetchInterval: isConnected ? false : 1000
    }
   );
 
