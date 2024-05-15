e7c48a4c1ab853a1d70f8dae981e7c4afd991f0f
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-15 15:54:03 +0200

    added ability to video and audio calls

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,6 +2,7 @@ import { Hash } from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
 import { SocketIndicator } from "@/components/socket-indicator";
+import { ChatVideoButton } from "./chat-video-button";
 
 interface ChatHeaderProps {
  serverId: string;
@@ -10,11 +11,18 @@ interface ChatHeaderProps {
  imageUrl?: string;
 }
 
-export const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
+export const ChatHeader = ({
+ serverId,
+ name,
+ type,
+ imageUrl
+}: ChatHeaderProps) => {
  return (
   <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
    <MobileToggle serverId={serverId} />
-   {type === "channel" && <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />}
+   {type === "channel" && (
+    <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+   )}
    {type === "conversation" && (
     <UserAvatar
      src={imageUrl}
@@ -23,6 +31,7 @@ export const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) 
    )}
    <p className="font-semibold text-md text-black dark:text-white">{name}</p>
    <div className="ml-auto flex items-center">
+    {type === "conversation" && <ChatVideoButton />}
     <SocketIndicator />
    </div>
   </div>
