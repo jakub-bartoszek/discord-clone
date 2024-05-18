18011d4e306f30b8888f58fc7e7d3a000a86a802
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-18 16:02:31 +0200

    sending post request when joining voice channel

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -7,6 +7,8 @@ import { Edit, Hash, Lock, Mic, Trash } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { ActionTooltip } from "@/components/action-tooltip";
 import { ModalType, useModal } from "@/hooks/use-modal-store";
+import { useParticipantsQuery } from "@/hooks/use-participants-query";
+import { ChannelParticipant } from "@/components/channel/channel-participant";
 
 interface ServerChannelProps {
  channel: Channel;
@@ -19,15 +21,47 @@ const iconMap = {
  [ChannelType.AUDIO]: Mic
 };
 
-export const ServerChannel = ({ server, channel, role }: ServerChannelProps) => {
+export const ServerChannel = ({
+ server,
+ channel,
+ role
+}: ServerChannelProps) => {
  const params = useParams();
  const router = useRouter();
+
+ const channelId = channel.id;
+
+ const queryKey = `channel:${channelId}:participants`;
+ const apiUrl = "/api/participants";
+
+ const { data } = useParticipantsQuery({ queryKey, apiUrl, channelId });
 
  const { onOpen } = useModal();
 
  const Icon = iconMap[channel.type];
 
- const onClick = () => {
+ const onClick = async () => {
+  if (channel.type === ChannelType.AUDIO) {
+   try {
+    const response = await fetch(`/api/socket/participants`, {
+     method: "POST",
+     headers: {
+      "Content-Type": "application/json"
+     },
+     body: JSON.stringify({
+      serverId: server.id,
+      channelId: channel.id
+     })
+    });
+
+    if (!response.ok) {
+     throw new Error("Failed to join voice channel");
+    }
+   } catch (error) {
+    console.error(error);
+   }
+  }
+
   router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
  };
 
@@ -37,42 +71,55 @@ export const ServerChannel = ({ server, channel, role }: ServerChannelProps) => 
  };
 
  return (
-  <button
-   onClick={onClick}
-   className={cn(
-    "group p-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
-    params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
-   )}
-  >
-   <Icon className="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
-   <p
+  <>
+   <button
+    onClick={onClick}
     className={cn(
-     "line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-     params?.channelId === channel.id &&
-      "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+     "group p-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+     params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
     )}
    >
-    {channel.name}
-   </p>
-   {channel.name !== "general" && role !== MemberRole.GUEST && (
-    <div className="ml-auto flex items-center- gap-x-2">
-     <ActionTooltip label="Edit">
-      <Edit
-       onClick={(e) => onAction(e, "editChannel")}
-       className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+    <Icon className="flex-shrink-0 w-5 h-5 text-zinc-500 dark:text-zinc-400" />
+    <p
+     className={cn(
+      "line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
+      params?.channelId === channel.id &&
+       "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+     )}
+    >
+     {channel.name}
+    </p>
+    {channel.name !== "general" && role !== MemberRole.GUEST && (
+     <div className="ml-auto flex items-center- gap-x-2">
+      <ActionTooltip label="Edit">
+       <Edit
+        onClick={(e) => onAction(e, "editChannel")}
+        className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+       />
+      </ActionTooltip>
+      <ActionTooltip label="Delete">
+       <Trash
+        onClick={(e) => onAction(e, "deleteChannel")}
+        className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+       />
+      </ActionTooltip>
+     </div>
+    )}
+    {channel.name === "general" && (
+     <Lock className="ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400" />
+    )}
+   </button>
+   {channel.type === "AUDIO" && data && data?.items?.length !== 0 && (
+    <>
+     {data.items.map((member: any) => (
+      <ChannelParticipant
+       key={member.id}
+       member={member}
+       server={server}
       />
-     </ActionTooltip>
-     <ActionTooltip label="Delete">
-      <Trash
-       onClick={(e) => onAction(e, "deleteChannel")}
-       className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
-      />
-     </ActionTooltip>
-    </div>
+     ))}
+    </>
    )}
-   {channel.name === "general" && (
-    <Lock className="ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400" />
-   )}
-  </button>
+  </>
  );
 };
