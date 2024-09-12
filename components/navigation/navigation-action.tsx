5a65b0c12de8da8676d94ddf61d3d7b04e416587
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-09-12 16:09:38 +0200

    change: changed background and text colors

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -19,11 +19,11 @@ export const NavigationAction = () => {
      onClick={() => onOpen("createServer")}
      className="group flex items-center"
     >
-     <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-zinc-100 dark:bg-neutral-700 group-hover:bg-emerald-500">
+     <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-[#250f4a] group-hover:bg-[#eb07ff]">
       <Plus
-       className="group-hover:text-white transition text-emerald-500"
+       className="group-hover:text-white transition text-[#eb07ff]"
        size={25}
-      ></Plus>
+      />
      </div>
     </button>
    </ActionTooltip>
