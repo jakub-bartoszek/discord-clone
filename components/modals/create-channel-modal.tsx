2e57c7744575c24fa7dbe1ee5a35acd3dadef4a0
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-09-13 15:33:58 +0200

    change: changed create channel modal styles

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -101,7 +101,7 @@ export const CreateChannelModal = () => {
    open={isModalOpen}
    onOpenChange={handleClose}
   >
-   <DialogContent className="bg-white text-black p-0 overflow-hidden">
+   <DialogContent>
     <DialogHeader className="pt-8 px-6">
      <DialogTitle className="text-2xl text-center font-bold">
       Customize channel
@@ -118,13 +118,13 @@ export const CreateChannelModal = () => {
         name="name"
         render={({ field }) => (
          <FormItem>
-          <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+          <FormLabel className="font-bold">
            Channel name
           </FormLabel>
           <FormControl>
            <Input
             disabled={isLoading}
-            className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible: ring-offset-0"
+            className="bg-black/50 border-0 focus-visible:ring-0focus-visible: ring-offset-0"
             placeholder="Enter channel name"
             {...field}
            />
@@ -145,14 +145,15 @@ export const CreateChannelModal = () => {
            defaultValue={field.value}
           >
            <FormControl>
-            <SelectTrigger className="bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none">
+            <SelectTrigger className="bg-black/50 border-0 focus:ring-0 ring-offset-0 focus:ring-offset-0 capitalize outline-none">
              <SelectValue placeholder="Select a channel type" />
-             <SelectContent>
+             <SelectContent
+             className="bg-black/50 backdrop-blur-lg border-none">
               {Object.values(ChannelType).map((type) => (
                <SelectItem
+               className="hover:bg-white/10 rounded-md cursor-pointer"
                 key={type}
                 value={type}
-                className="capitalize"
                >
                 {type.toLowerCase()}
                </SelectItem>
@@ -166,7 +167,7 @@ export const CreateChannelModal = () => {
         )}
        />
       </div>
-      <DialogFooter className="bg-gray-100 px-6 py-4">
+      <DialogFooter className="px-6 py-4">
        <Button
         disabled={isLoading}
         variant={"primary"}
