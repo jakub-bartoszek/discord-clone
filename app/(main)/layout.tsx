8ec20555df55cdada7e5678a29b3c31d99443fae
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-09-19 13:05:38 +0200

    change: changed padding in md screens

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,7 +2,7 @@ import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
  return (
-  <div className="h-full flex p-2 gap-2">
+  <div className="h-full flex md:p-2 gap-2">
    <div className="hidden md:flex h-full w-[72px] z-30 flex-col">
     <NavigationSidebar />
    </div>
