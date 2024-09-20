b6161e67bf170dc19fdf73d54da57cd9ede448d6
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-09-20 15:57:30 +0200

    change: changed mobile sidebar styles

--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -8,13 +8,16 @@ export const MobileToggle = ({ serverId }: { serverId: string }) => {
  return (
   <Sheet>
    <SheetTrigger asChild>
-    <Button variant="ghost" className="p-0 mr-2 md:hidden">
+    <Button
+     variant="ghost"
+     className="p-0 mr-2 md:hidden"
+    >
      <Menu />
     </Button>
    </SheetTrigger>
    <SheetContent
     side="left"
-    className="p-0 flex gap-0"
+    className="p-0 flex gap-0 bg-[#2a0a51]/50 backdrop-blur-lg border-none"
    >
     <div className="w-[72px]">
      <NavigationSidebar />
--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -3,7 +3,6 @@
 import * as React from "react";
 import * as SheetPrimitive from "@radix-ui/react-dialog";
 import { cva, type VariantProps } from "class-variance-authority";
-import { X } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
@@ -66,10 +65,6 @@ const SheetContent = React.forwardRef<
    {...props}
   >
    {children}
-   <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-    <X className="h-4 w-4" />
-    <span className="sr-only">Close</span>
-   </SheetPrimitive.Close>
   </SheetPrimitive.Content>
  </SheetPortal>
 ));
@@ -106,7 +101,7 @@ const SheetTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
  <SheetPrimitive.Title
   ref={ref}
-  className={cn("text-lg font-semibold text-foreground", className)}
+  className={cn("text-lg font-semibold", className)}
   {...props}
  />
 ));
@@ -118,7 +113,7 @@ const SheetDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
  <SheetPrimitive.Description
   ref={ref}
-  className={cn("text-sm text-muted-foreground", className)}
+  className={cn("text-sm", className)}
   {...props}
  />
 ));
